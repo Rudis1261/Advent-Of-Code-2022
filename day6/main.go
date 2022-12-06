b40ba9ceeadbb57d83f7c910835ec1b91c0b4bc9
Rudi Strydom <rudi.strydom@xneelo.com>
2022-12-06 07:56:20 +0200

    Day 6 complete

--- day6/main.go
+++ day6/main.go
@@ -3,13 +3,48 @@ package main
 import (
 	"log"
 	"os"
-	"strings"
 )
 
 func main() {
 	data, _ := os.ReadFile("input.txt")
-	lines := strings.Split(string(data), "\n")
+	chars := string(data)
+	first := findFirstUnique(chars, 4)
+	message := findFirstUnique(chars, 14)
 
-	log.Printf("Result 1: %s\n", lines)
-	log.Printf("Result 2: %s\n", lines)
+	log.Printf("Result 1: %d\n", first)
+	log.Printf("Result 2: %v\n", message)
+}
+
+func findFirstUnique(input string, size int) int {
+	for i := size; i < len(input); i++ {
+		isFound := unique(input[i-size : i])
+		if isFound {
+			return i
+		}
+	}
+	return 0
+}
+
+func findAllUnique(input string, size int) map[int]string {
+	uniques := make(map[int]string)
+	for i := size; i < len(input); i++ {
+		isFound := unique(input[i-size : i])
+		if isFound {
+			uniques[i] = input[i-size : i]
+		}
+	}
+	return uniques
+}
+
+func unique(abc string) bool {
+	values := make(map[rune]bool)
+	for _, i := range abc {
+		_, ok := values[i]
+		if ok {
+			return false
+		}
+
+		values[i] = true
+	}
+	return true
 }
--- day6/main_test.go
+++ day6/main_test.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_main(t *testing.T) {
 	// assert := assert.New(t)
+}
+
+func Test_findFirstUnique(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(5, findFirstUnique("bvwbjplbgvbhsrlpgdmjqwftvncz", 4))
+	assert.Equal(6, findFirstUnique("nppdvjthqldpwncqszvftbrmjlhg", 4))
+	assert.Equal(10, findFirstUnique("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4))
+	assert.Equal(11, findFirstUnique("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4))
 }
