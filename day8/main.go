d434a0701d57cc8e977902e10e532b4f49884825
Rudi Strydom <rudi.strydom@xneelo.com>
2022-12-08 11:22:29 +0200

    Abomonation

--- day8/main.go
+++ day8/main.go
@@ -3,13 +3,151 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	data, _ := os.ReadFile("input.txt")
 	lines := strings.Split(string(data), "\n")
+	count, scenicScore := getCount(lines)
 
-	log.Printf("Result 1: %s\n", lines)
-	log.Printf("Result 2: %s\n", lines)
+	log.Printf("Result 1: %d\n", count)
+	log.Printf("Result 2: %d\n", scenicScore)
+}
+
+func getCount(lines []string) (int, int) {
+	canSee := 0
+	maxScenic := 0
+	for y := 1; y < len(lines)-1; y++ {
+		for x := 1; x < len(lines[y])-1; x++ {
+			if visible(lines, x, y) {
+				canSee++
+			}
+
+			scenicScore := scenicScore(lines, x, y)
+			if scenicScore > maxScenic {
+				maxScenic = scenicScore
+			}
+		}
+	}
+
+	totalVisible := (len(lines[0]) * 2) + ((len(lines) - 2) * 2) // along the edge
+	return totalVisible + canSee, maxScenic
+}
+
+func visible(lines []string, x int, y int) bool {
+	horizontal := lines[y]
+	currentValue, _ := strconv.Atoi(string(lines[y][x]))
+	var vertical string
+
+	for i := 0; i < len(lines); i++ {
+		vertical += string(lines[i][x])
+	}
+
+	xMin := horizontal[0:x]
+	xMax := horizontal[x+1:]
+	yMin := vertical[0:y]
+	yMax := vertical[y+1:]
+
+	xMinVisible := true
+	xMaxVisible := true
+	yMinVisible := true
+	yMaxVisible := true
+
+	for _, val := range xMin {
+		adjacentTree, _ := strconv.Atoi(string(val))
+		if currentValue <= adjacentTree {
+			xMinVisible = false
+		}
+	}
+
+	for _, val := range xMax {
+		adjacentTree, _ := strconv.Atoi(string(val))
+		if currentValue <= adjacentTree {
+			xMaxVisible = false
+		}
+	}
+
+	for _, val := range yMin {
+		adjacentTree, _ := strconv.Atoi(string(val))
+		if currentValue <= adjacentTree {
+			yMinVisible = false
+		}
+	}
+
+	for _, val := range yMax {
+		adjacentTree, _ := strconv.Atoi(string(val))
+		if currentValue <= adjacentTree {
+			yMaxVisible = false
+		}
+	}
+
+	return xMaxVisible || xMinVisible || yMinVisible || yMaxVisible
+}
+
+func scenicScore(lines []string, x int, y int) int {
+	horizontal := lines[y]
+	currentValue, _ := strconv.Atoi(string(lines[y][x]))
+	var vertical string
+	for i := 0; i < len(lines); i++ {
+		vertical += string(lines[i][x])
+	}
+
+	xMin := horizontal[0:x]
+	xMax := horizontal[x+1:]
+	yMin := vertical[0:y]
+	yMax := vertical[y+1:]
+
+	xMinScenicScore := 0
+	xMaxScenicScore := 0
+	yMinScenicScore := 0
+	yMaxScenicScore := 0
+
+	for i := len(xMin) - 1; i >= 0; i-- {
+		adjacentTree, _ := strconv.Atoi(string(xMin[i]))
+		xMinScenicScore++
+		if currentValue <= adjacentTree {
+			break
+		}
+	}
+
+	for i := 0; i < len(xMax); i++ {
+		adjacentTree, _ := strconv.Atoi(string(xMax[i]))
+		xMaxScenicScore++
+		if currentValue <= adjacentTree {
+			break
+		}
+	}
+
+	for i := len(yMin) - 1; i >= 0; i-- {
+		adjacentTree, _ := strconv.Atoi(string(yMin[i]))
+		yMinScenicScore++
+		if currentValue <= adjacentTree {
+			break
+		}
+	}
+
+	for i := 0; i < len(yMax); i++ {
+		adjacentTree, _ := strconv.Atoi(string(yMax[i]))
+		yMaxScenicScore++
+		if currentValue <= adjacentTree {
+			break
+		}
+	}
+
+	if xMinScenicScore == 0 {
+		xMinScenicScore = 1
+	}
+	if xMaxScenicScore == 0 {
+		xMaxScenicScore = 1
+	}
+	if yMinScenicScore == 0 {
+		yMinScenicScore = 1
+	}
+	if yMaxScenicScore == 0 {
+		yMaxScenicScore = 1
+	}
+
+	return yMinScenicScore * xMinScenicScore * xMaxScenicScore * yMaxScenicScore
 }
--- day8/main_test.go
+++ day8/main_test.go
@@ -2,8 +2,66 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_main(t *testing.T) {
-	// assert := assert.New(t)
+func Test_getCount(t *testing.T) {
+	assert := assert.New(t)
+	sampleData := []string{
+		"30373",
+		"25512",
+		"65332",
+		"33549",
+		"35390",
+	}
+	count, scenicScore := getCount(sampleData)
+	assert.Equal(21, count)
+	assert.Equal(8, scenicScore)
+}
+
+func Test_tallestAmongsOthers(t *testing.T) {
+	assert := assert.New(t)
+	sampleData := []string{
+		"30373",
+		"25512",
+		"65332",
+		"33549",
+		"35390",
+	}
+
+	assert.Equal(false, visible(sampleData, 3, 1))
+	assert.Equal(false, visible(sampleData, 1, 3))
+	assert.Equal(false, visible(sampleData, 2, 2))
+	assert.Equal(false, visible(sampleData, 3, 3))
+
+	assert.Equal(true, visible(sampleData, 1, 1))
+	assert.Equal(true, visible(sampleData, 1, 2))
+	assert.Equal(true, visible(sampleData, 2, 1))
+	assert.Equal(true, visible(sampleData, 2, 3))
+	assert.Equal(true, visible(sampleData, 3, 2))
+}
+
+func Test_scenicScore(t *testing.T) {
+	assert := assert.New(t)
+	sampleData := []string{
+		"30373",
+		"25512",
+		"65332",
+		"33549",
+		"35390",
+	}
+	type test struct {
+		X     int
+		Y     int
+		Score int
+	}
+	tests := []test{
+		{X: 2, Y: 1, Score: 4},
+		{X: 2, Y: 3, Score: 8},
+	}
+
+	for _, testCase := range tests {
+		assert.Equal(testCase.Score, scenicScore(sampleData, testCase.X, testCase.Y))
+	}
 }
